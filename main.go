d73f5e5aa51feb752e69a829190d6af36bcad089
Kushro <kushro@pm.me>
2019-07-02 13:02:37 -0600

    -everything improved -added two checking modes to the code -complete check mode isn't working properly yet -simple mode working flawless

--- main.go
+++ main.go
@@ -3,31 +3,78 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
+	"net/url"
 	"time"
 )
 
-// TODO:
-// ROUND MS TO 2 DECIMALS
-// OPEN A FILE, READ ITS CONTENTS AND DO A QUEUE/LIST AND THEN CHECK
-
-func main() {
-
-	var proxy string = "http://google.com"
-	timerStarted := time.Now()
-	resp, err := http.Get(proxy)
-	elapsedTime := time.Since(timerStarted)
-
-	var response string = fmt.Sprintf("%v", resp)
+//Simple check directly pinging the address provided
+func simpleCheck(proxyAddress string) *http.Response {
+	response, err := http.Get(proxyAddress)
 	if err != nil {
 		fmt.Println("Couldn't reach the address")
 	}
-	var checkstatus bool = strings.Contains(response, "200 OK")
+	return response
 
-	if checkstatus {
-		fmt.Printf("200 OK --> Working proxy with a speed of %s\n", elapsedTime)
+}
+
+//Isn't detecting the proxy and getting through, doing a direct connection
+//to the checking address, fix this ASAP.
+func completeCheck(proxyAddress string, checkingAddress string) *http.Response {
+	proxyUrl, err := url.Parse(proxyAddress)
+	if err != nil {
+		fmt.Println(proxyUrl) // <nil> ...this is the problem... parsing not working?
+		fmt.Println("Couldn't reach the proxy")
+	}
+	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+	response, err := myClient.Get(checkingAddress)
+	if err != nil {
+		fmt.Println("Couldn't reach the address")
+	}
+	return response
+}
+
+//Ok, this is the main problem right now
+//this should retrieve a string with the Status Code
+//of the http.Response assigned to "response"
+func statusCheck(response *http.Response) bool {
+	if response.StatusCode == http.StatusOK {
+		return true
 	} else {
-		fmt.Println("Unknown Response --> Unknown proxy state")
+		return false
+	}
+	//This doesn't work for some reason... figure it out for a more
+	//specific status checking using the recieved response body
+	/*
+		var bodyString string
+		if response.StatusCode == http.StatusOK {
+			bodyBytes, err := ioutil.ReadAll(response.Body)
+			bodyString := string(bodyBytes)
+			if err != nil {
+				fmt.Println("Error reading body of the response:", err)
+			}
+			//statusString := string(response.StatusCode)
+		}
+		return bodyString
+	*/
+}
+
+//Fixed, now turn it into a proper .txt checker and writter
+func main() {
+
+	var proxyAddress string = "149.56.133.165:3128"
+	var checkingAddress string = "http://google.com"
+	timerStarted := time.Now()
+	//resp := simpleCheck(proxyAddress)
+	resp := completeCheck(proxyAddress, checkingAddress)
+	chk := statusCheck(resp)
+	elapsedTime := time.Since(timerStarted)
+
+	//var checkstatus bool = strings.Contains(response, "200 OK")
+
+	if chk {
+		fmt.Printf("200 OK --> Working proxy | %s\n", elapsedTime)
+	} else {
+		fmt.Printf("Proxy not Working | %d\n", resp.StatusCode)
 	}
 
 }
